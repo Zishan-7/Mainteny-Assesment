2728cf075bf56207815c2d7f8e96dab68b109f6f
Zishan-7 <zishan2539@gmail.com>
2022-01-15 18:00:18 +0530

    Added Topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Topbar from "./Components/Topbar/Topbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Topbar />
+    </>
+  );
 }
 
 export default App;
